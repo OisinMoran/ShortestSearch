b8caa369680c083c8b2711291a8c2926ac42590e
Oisín Moran <OisinMoran@users.noreply.github.com>
2017-03-05 00:01:16 +0000

    Added API Usage
    Program actually searches now

--- shortest_search.py
+++ shortest_search.py
@@ -1,11 +1,19 @@
-# NOTE: This does not search or use Google,
-#       it only generates search suggestions
-from bs4 import BeautifulSoup
+from googleapiclient.discovery import build
 from urllib.request import Request, urlopen
 from collections import Counter
+from bs4 import BeautifulSoup
+import configparser
 import itertools
 import re
-import time
+
+## Read configuration file for API access details
+config = configparser.ConfigParser()
+config.read('CSE_config.ini')
+# Get custom search id and developer key from config file
+# Get your own at:
+# https://developers.google.com/custom-search/json-api/v1/overview
+my_cse_id = config['CSE']['cse_id']
+dev_key = config['CSE']['dev_key']
 
 MIN_WORDS_IN_PHRASE = 2
 MAX_WORDS_IN_PHRASE = 2
@@ -21,6 +29,12 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+# Function to search Google using their CSE API
+def google_search(search_term, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=dev_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
 
 ## Get frequency of 20k most common English words
 # File from https://github.com/first20hours/google-10000-english
@@ -74,8 +88,6 @@ sorted_words = sorted(word_scores, key=word_scores.get, reverse=True)
 # print(sorted_words)
 print("{} words found on page.".format(len(sorted_words)))
 
-# Manual example of results that work
-hits = ['TED VEST', 'TED EAGLEMAN', 'TED SENSES']
 min_len = float('inf')
 search_no = 1
 
@@ -83,11 +95,12 @@ print("Search Suggestions:")
 ## Generate search phrases
 for L in range(MIN_WORDS_IN_PHRASE, MAX_WORDS_IN_PHRASE+1):
     for subset in itertools.combinations(sorted_words, L):
-        phrase = "{} {}".format(subset[0], subset[1])
-        if len(phrase) <= min_len:
-            print("{}: {}".format(search_no, phrase))
+        search_phrase = "{} {}".format(subset[0], subset[1])
+        if len(search_phrase) <= min_len:
+            results = google_search(search_phrase, my_cse_id, num=1)
+            for result in results:
+                first_url = result.get('link')
             search_no += 1
-            if phrase in hits:
-                print('-AW NICE-')
-                min_len = len(phrase)
-            time.sleep(0.2)
+            if first_url == given_url:
+                print("{}: {}".format(search_no, search_phrase))
+                min_len = len(search_phrase)
